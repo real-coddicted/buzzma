585f9ce97b667d85071f9faccb0c46df96837f9e
Payam Rastogi <payam@coddicted.com>
2026-05-05 08:54:22 -0400

    make url consistent

--- backend/src/main/java/com/coddicted/buzzma/identity/service/SecurityQuestionAnswerService.java
+++ backend/src/main/java/com/coddicted/buzzma/identity/service/SecurityQuestionAnswerService.java
@@ -7,7 +7,6 @@ import java.util.List;
 import java.util.UUID;
 import org.springframework.stereotype.Service;
 
-@Service
 public interface SecurityQuestionAnswerService {
 
   List<SecurityQuestion> listSecurityQuestions();
