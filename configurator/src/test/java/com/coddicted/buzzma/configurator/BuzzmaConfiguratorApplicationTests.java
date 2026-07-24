a98910c3a3f9cef4d8f34cd601618a3e128993d6
Anupam Jain <anupam2204.smart@gmail.com>
2026-07-24 21:53:22 +0530

    Issue#494 Fix: Build failure

--- configurator/src/test/java/com/coddicted/buzzma/configurator/BuzzmaConfiguratorApplicationTests.java
+++ configurator/src/test/java/com/coddicted/buzzma/configurator/BuzzmaConfiguratorApplicationTests.java
@@ -1,16 +1,18 @@
 package com.coddicted.buzzma.configurator;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
 import org.springframework.test.context.ActiveProfiles;
 import org.testcontainers.containers.PostgreSQLContainer;
 import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
 
-@SpringBootTest
-@ActiveProfiles("test")
+@DataJpaTest
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
 @Testcontainers
+@ActiveProfiles("test")
 class BuzzmaConfiguratorApplicationTests {
 
   @Container @ServiceConnection
