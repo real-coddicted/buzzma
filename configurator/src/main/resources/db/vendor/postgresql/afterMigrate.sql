38d97006f805473ecbedf0352af0beecf8e37ddd
anupam <anupam@coddicted.com>
2026-08-15 11:45:51 -0400

    anupam/issue-643/deploy-config-correction (#644)
    Co-authored-by: Anupam Jain <anupam2204.smart@gmail.com>
    Reviewed-on: https://gitea.local.coddicted.com/coddicted/buzzma/pulls/644

---
+++ configurator/src/main/resources/db/vendor/postgresql/afterMigrate.sql
@@ -0,0 +1,8 @@
+DO $$
+BEGIN
+  IF '${appRuntimeRole}' <> '' THEN
+    EXECUTE 'GRANT SELECT, INSERT, UPDATE, REFERENCES, TRIGGER ON ALL TABLES IN SCHEMA ${flyway:defaultSchema} TO ' || quote_ident('${appRuntimeRole}');
+    EXECUTE 'GRANT USAGE, SELECT ON ALL SEQUENCES IN SCHEMA ${flyway:defaultSchema} TO ' || quote_ident('${appRuntimeRole}');
+  END IF;
+END
+$$;
